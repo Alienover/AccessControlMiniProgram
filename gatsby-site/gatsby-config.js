f99581cf8566207a292600359de59a6b4c35ff0d
jiarong <jiarong@edison.tech>
2019-11-23 14:06:19 +0800

    Initial PWA for gatsby site

---
+++ gatsby-site/gatsby-config.js
@@ -0,0 +1,29 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+module.exports = {
+  /* Your site config here */
+  plugins: [
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        name: "Office Control",
+        short_name: "Office",
+        icon: "src/medias/icon.png",
+        start_url: "/",
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-offline",
+      options: {
+        precachePages: ["/"],
+      },
+    },
+  ],
+}
